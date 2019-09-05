792a6d91ffb4a2980624a2eca153e52c0986dcf5
Leon Linton <leon.linton@live.co.uk>
2019-09-05 19:13:01 +0100

    Adds Cart class

---
+++ src/cart.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./cart.css";
+
+class Cart extends React.Component {
+  render() {
+    return (
+      <div>
+        <h3>Cart</h3>
+      </div>
+    );
+  }
+}
+
+export default Cart;
