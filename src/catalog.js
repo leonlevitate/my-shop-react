13f48be9810053b7fe94b5c5b0deb1f0aed1da3b
Leon Linton <leon.linton@live.co.uk>
2019-09-05 18:26:43 +0100

    Sets up Catalog class

---
+++ src/catalog.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./catalog.css";
+
+class Catalog extends React.Component {
+  render() {
+    return (
+      <div>
+        <h2>Catalog</h2>
+      </div>
+    );
+  }
+}
+
+export default Catalog;
