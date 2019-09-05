56fd54d5ef1af63734c9c69da84500da493658a2
Leon Linton <leon.linton@live.co.uk>
2019-09-05 21:15:58 +0100

    Adds Shopping cart component

---
+++ src/CartList.js
@@ -0,0 +1,18 @@
+import React from "react";
+class CartList extends React.Component {
+  render() {
+    return (
+      <div>
+        <h3>Shopping Cart:</h3>
+        <ul>
+          <p>Traditional Merlot</p>
+        </ul>
+        <ul>
+          <p>Classic Chianti</p>
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default CartList;
--- src/cart.js
+++ src/cart.js
@@ -1,11 +1,12 @@
 import React from "react";
 import "./cart.css";
+import CartList from "./CartList";
 
 class Cart extends React.Component {
   render() {
     return (
       <div>
-        <h3>Cart</h3>
+        <CartList />
       </div>
     );
   }
